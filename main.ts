b525a6841fc3539b328f54fa24e3adf116d6f859
Alex Matseshe <amatseshe@gmail.com>
2023-12-05 20:12:12 -0600

    fix: typo

--- main.ts
+++ main.ts
@@ -10,4 +10,4 @@ import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
 
-await start(manifest, config;
+await start(manifest, config);
